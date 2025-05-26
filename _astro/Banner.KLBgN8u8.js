5fdb081f0b783fa313a566105178644d2d09f96f
AgungDevlop <AgungDevlop@users.noreply.github.com>
2025-05-26 19:39:00 +0000

    deploy: ccaa04f86ab27b894a545d2ff2a480b302e4206b

---
+++ _astro/Banner.KLBgN8u8.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime.D_zvdyIk.js";import"./index.6otl1p8d.js";const i=()=>e.jsxs("section",{className:"py-10 bg-black text-white",children:[e.jsxs("div",{className:"container mx-auto px-2 sm:px-6 lg:px-8 min-h-32 sm:min-h-48 lg:min-h-64 flex flex-col justify-center text-left","data-aos":"fade-in",children:[e.jsx("h1",{className:"text-2xl sm:text-4xl lg:text-5xl font-[Bebas Neue] font-bold tracking-wide whitespace-pre mb-2 sm:mb-4","data-aos":"fade-up","data-aos-delay":"100",children:"K I T A  J O K I  J A S A".split("").map((t,a)=>e.jsx("span",{className:"animated-letter",style:{animationDelay:`${a*.3}s`},children:t},a))}),e.jsx("p",{className:"text-sm sm:text-base lg:text-lg font-[Inter] text-gray-400 max-w-md","data-aos":"fade-up","data-aos-delay":"200",children:"Penyedia jasa profesional untuk website, aplikasi, desain grafis, dan solusi teknologi."})]}),e.jsx("style",{jsx:!0,children:`
+        .animated-letter {
+          animation: colorChange 8s infinite ease-in-out;
+          display: inline-block;
+        }
+        @keyframes colorChange {
+          0% { color: #FBBF24; }
+          20% { color: #FFFFFF; }
+          40% { color: #D1D5DB; }
+          60% { color: #60A5FA; }
+          80% { color: #F59E0B; }
+          100% { color: #93C5FD; }
+        }
+      `})]});export{i as default};
